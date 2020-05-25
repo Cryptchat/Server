54bf7f6fdb71bef123492aea0ef3895ae6ec5c66
OsamaSayegh <asooomaasoooma90@gmail.com>
2020-05-25 23:18:01 +0300

    Serializers for realz

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class UserSerializer < ActiveModel::Serializer
+  attributes(
+    :id,
+    :name,
+    :country_code,
+    :phone_number,
+    :identity_key,
+    :updated_at,
+    :created_at
+  )
+
+  def id
+    2147483
+  end
+
+  def updated_at
+    object.updated_at.to_f
+  end
+
+  def created_at
+    object.created_at.to_f
+  end
+end
